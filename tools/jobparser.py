a1020286573f12c2370c6e4bdc7f4a8b4cb4145a
jonijasper <jasper.alakotila@protonmail.com>
2025-07-05 13:20:12 +0300

    comments

--- tools/jobparser.py
+++ tools/jobparser.py
@@ -8,6 +8,7 @@ class JobParser(HTMLParser):
                 "data-category": "category", 
                 "data-company": "company", 
                 "href": "url"}
+    # for matching relevant tags and attributes
     JOB_TAG = "a"
     JOB_ATTR = ("class", "job-box__hover gtm-search-result")
     NEXT_TAG = "link"
@@ -15,13 +16,13 @@ class JobParser(HTMLParser):
     
     def __init__(self):
         HTMLParser.__init__(self)
-        self.alljobs = {"title": [], "category": [], "company": [], "url": []}
+        self.alljobs = {value: [] for value in self.JOBINFO.values()}
         self.nextpage = None
 
     def handle_starttag(self, tag, attrs):
+        # tags containing job information
         if tag == self.JOB_TAG and self.JOB_ATTR in attrs:
-            # found job tag
-            job = {"title": "", "category": "", "company": "", "url": ""}
+            job = {key: "" for key in self.alljobs.keys()}
             # specs are in tag attributes:
             for attr,value in attrs:
                 if attr in self.JOBINFO:
@@ -30,13 +31,16 @@ class JobParser(HTMLParser):
             # data-job-slug="job-title-foobar-12345"
             title = job["title"].split('-')
             job["title"] = " ".join(title[:-2])
+
+            # href="/tyopaikat/tyo/...."
             job["url"] = "https://duunitori.fi" + job["url"]
 
+            # add job to list
             for key,value in job.items():
                 self.alljobs[key].append(value)
 
-        if tag == self.NEXT_TAG and self.NEXT_ATTR in attrs:
-            # tag has link to the next page in "href" attribute
+        # tag containing link to the next page
+        elif tag == self.NEXT_TAG and self.NEXT_ATTR in attrs:
             for attr,value in attrs:
                 if attr == "href":
                     self.nextpage = value
